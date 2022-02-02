ab54cf7fde59c4ba0ce7b7653af7a2cb0568cd65
leandrojaviercepeda <leandrojaviercepeda@gmail.com>
2022-02-02 12:37:52 -0300

    m3u3:
    en progreso:
    *Ejericio 1:
    - Crear el código que dada una distancia determine el medio de transporte apropiado de acuerdo a las siguientes reglas:
    0 a 1000 metros = pie
    1000 a 10000 metros = bicicleta
    10000 a 30000 metros = colectivo
    30000 a 100000 metros = auto
    +100000 = avion
    
    * Ejericio 2:
    - Crear el código que reciba como parámetro un array de números y devuelva el mayor.

--- src/ej2.js
+++ src/ej2.js
@@ -2,3 +2,9 @@
  * Ejericio 2
  * Crear el código que reciba como parámetro un array de números y devuelva el mayor.
  */
+
+const lenght = 40;
+const array = Array.from({length: lenght}, () => Math.floor(Math.random() * 100));
+const getMax = (array) => Array.isArray(array) ? Math.max(...array) : "Error al obtener el maximo";
+console.log("array: ", array);
+console.log("El numero mayor del array es: ", getMax(array));
